b467b9d5f9228e72ee765f224000a84d42e6954c
Avinash Padmanabhan <avinashpadmanabhan@gmail.com>
2017-03-29 22:17:11 -0700

    second commit to tumblr ios app

---
+++ CodepathTumblr/CodepathTumblr/PhotoViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoViewCell.swift
+//  CodepathTumblr
+//
+//  Created by Padmanabhan, Avinash on 3/29/17.
+//  Copyright © 2017 Intuit. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
